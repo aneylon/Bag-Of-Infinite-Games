c7290cee1f3747030b293aac69386e2b5dc6bfd7
aneylon <arlen.m.neylon@gmail.com>
2025-11-04 16:21:34 -0700

    Add basic routes and postman tests.

--- boig-back/index.js
+++ boig-back/index.js
@@ -3,7 +3,23 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Express!");
+  res.send("BoIG GET");
+});
+
+app.post("/", (req, res) => {
+  res.send("BoIG POST");
+});
+
+app.patch("/", (req, res) => {
+  res.send("BoIG PATCH");
+});
+
+app.put("/", (req, res) => {
+  res.send("BoIG PUT");
+});
+
+app.delete("/", (req, res) => {
+  res.send("BoIG DELETE");
 });
 
 app.listen(port, () => {
