eaa8ecd41d3cc4bb58224b2268e8c35f6fa12fa2
aneylon <arlen.m.neylon@gmail.com>
2025-11-25 07:54:49 -0700

    Add Materialize css.

--- boig-front/src/components/About/About.js
+++ boig-front/src/components/About/About.js
@@ -1,3 +1,15 @@
+import Contact from "../Contact/Contact";
+import Bugs from "../Bugs/SubmitNewBug";
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+        sit!
+      </p>
+      <Contact />
+      <Bugs />
+    </div>
+  );
 };
--- boig-front/src/components/Navbar/Navbar.js
+++ boig-front/src/components/Navbar/Navbar.js
@@ -3,10 +3,22 @@ import { NavLink } from "react-router";
 export const Navbar = () => {
   return (
     <nav>
-      <h2>Bag of Infinite Games</h2>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/news">News</NavLink>
+      <div className="nav-wrapper">
+        <NavLink to="/" className="brand-logo">
+          Bag of Infinite Games
+        </NavLink>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/news">News</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
