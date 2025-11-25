32a366af6be13b41993cc461e613150d3bbdc3d7
aneylon <arlen.m.neylon@gmail.com>
2025-11-24 19:17:16 -0700

    Add new component framework and script for updating version.

---
+++ boig-front/src/components/Bugs/SubmitNewBug.js
@@ -0,0 +1,9 @@
+const SubmitNewBug = () => {
+  return (
+    <div>
+      <h1>New Bug</h1>
+    </div>
+  );
+};
+
+export default SubmitNewBug;
--- boig-front/tools/generate-version-file.js
+++ boig-front/tools/generate-version-file.js
@@ -1,1 +1,26 @@
 console.log("generate version file");
+
+const { writeFile } = require("fs");
+
+const path = require("path");
+
+const packageJson = require(`${process.cwd()}/package.json`);
+
+const newFilePath = path.join(process.cwd(), "public", "version.json");
+
+const appVersion = packageJson.version;
+
+const jsonData = {
+  version: appVersion,
+};
+
+const jsonContent = JSON.stringify(jsonData);
+
+writeFile(newFilePath, jsonContent, "utf-8", function (error) {
+  if (error) {
+    console.error(error);
+    throw new Error(error);
+  } else {
+    console.log("version file saved with v", appVersion);
+  }
+});
