a92607ad9f81b5d83a40e46f15af472df0bc0e0c
aneylon <arlen.m.neylon@gmail.com>
2025-12-31 19:23:39 -0700

    add bug form.

--- boig-front/src/components/Bugs/SubmitNewBug.js
+++ boig-front/src/components/Bugs/SubmitNewBug.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 const SubmitNewBug = () => {
+  const [newBug, setNewBug] = useState("");
+  const submitNewBug = () => {
+    console.log("send to server", newBug);
+    // send:
+    // text
+    // user id
+    // date / time
+    setNewBug("");
+  };
   return (
     <div>
       <h1>New Bug</h1>
+      <textarea
+        name="newBug"
+        id="newBug"
+        value={newBug}
+        onChange={(e) => setNewBug(e.target.value)}
+      ></textarea>
+      <button onClick={submitNewBug}>Submit bug</button>
     </div>
   );
 };
