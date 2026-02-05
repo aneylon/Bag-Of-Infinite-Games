52f20ed24da77e4efb6b896d1af27ce3c32e48f4
aneylon <arlen.m.neylon@gmail.com>
2026-02-04 20:21:01 -0700

    Add login and signup to back end.

---
+++ boig-back/Controllers/userController.js
@@ -0,0 +1,36 @@
+const User = require("../Models/User");
+const jwt = require("jsonwebtoken");
+
+const secret = process.env.secret;
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, secret, { expiresIn: "3d" });
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, id: user._id, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token, id: user._id });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  loginUser,
+  signupUser,
+};
--- boig-back/Routes/Auth.js
+++ boig-back/Routes/Auth.js
@@ -1,19 +1,9 @@
 const express = require("express");
+const { loginUser, signupUser } = require("../Controllers/userController");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("auth!!!");
-});
+router.post("/signin", loginUser);
 
-router.post("/signin", (req, res) => {
-  const { password, username } = req.body;
-  console.log({ password, username });
-  res.send(`signin ${password} ${username}`);
-});
-
-router.post("/signup", (req, res) => {
-  const { password, username } = req.body;
-  res.send(`signup ${password} ${username}`);
-});
+router.post("/signup", signupUser);
 
 module.exports = router;
