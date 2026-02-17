a9b4ec850234c6038630d09a3ec5cb7d51ebe603
aneylon <arlen.m.neylon@gmail.com>
2026-02-16 18:32:18 -0700

    continue work on direct message.

--- boig-front/src/components/MessageBoard/DirectMessage.js
+++ boig-front/src/components/MessageBoard/DirectMessage.js
@@ -1,18 +1,34 @@
 import { useAuthContext } from "../../hooks/useAuthContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const DirectMessage = ({ recipient }) => {
   const { user } = useAuthContext();
   const [newMessage, setNewMessage] = useState("");
   const sendNewMessage = () => {
+    let data = {
+      recipient,
+      sender: user.id,
+      newMessage,
+    };
+    console.log(data);
     // TODO: api call to send new direct message
     setNewMessage("");
   };
+  useEffect(() => {
+    // TODO : Get exisiting direct messages
+  }, []);
   return (
     <div>
       <h1>Direct Message</h1>
       <h2>New Message</h2>
-      <textarea name="newMessage" id="newMessage"></textarea>
+      <textarea
+        name="newMessage"
+        id="newMessage"
+        value={newMessage}
+        onChange={(e) => {
+          setNewMessage(e.target.value);
+        }}
+      ></textarea>
       <button onClick={sendNewMessage}>send new message</button>
     </div>
   );
